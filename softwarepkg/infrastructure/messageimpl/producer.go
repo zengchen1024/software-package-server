73a8b829f1fe9472e7c63f10955e09ad5d21a52b
zengchen1024 <chenzeng2@huawei.com>
2023-03-15 11:36:03 +0800

    bug

--- softwarepkg/infrastructure/messageimpl/producer.go
+++ softwarepkg/infrastructure/messageimpl/producer.go
@@ -32,7 +32,7 @@ func (p *producer) NotifyPkgAbandoned(e message.EventMessage) error {
 }
 
 func (p *producer) NotifyPkgAlreadyClosed(e message.EventMessage) error {
-	return send(p.topics.AbandonedSoftwarePkg, e)
+	return send(p.topics.AlreadyClosedSoftwarePkg, e)
 }
 
 func (p *producer) NotifyPkgIndirectlyApproved(e message.EventMessage) error {
