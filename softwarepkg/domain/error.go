93bebf4ae2abfb4a909f29d28b21c271d6a20573
zengchen1024 <chenzeng2@huawei.com>
2023-05-10 14:26:03 +0800

    error code about re-run ci

--- softwarepkg/domain/error.go
+++ softwarepkg/domain/error.go
@@ -3,16 +3,22 @@ package domain
 import "errors"
 
 const (
-	errorSoftwarePkgNotImporter = "software_pkg_not_importer"
+	codeSoftwarePkgNotImporter = "software_pkg_not_importer"
+	codeSoftwarePkgCIIsRunning = "software_pkg_ci_is_running"
 )
 
 var (
+	errorCIIsRunning    = errors.New("ci is running")
 	errorNotTheImporter = errors.New("not the importer")
 )
 
 func ParseErrorCode(err error) string {
 	if errors.Is(err, errorNotTheImporter) {
-		return errorSoftwarePkgNotImporter
+		return codeSoftwarePkgNotImporter
+	}
+
+	if errors.Is(err, errorCIIsRunning) {
+		return codeSoftwarePkgCIIsRunning
 	}
 
 	return ""
